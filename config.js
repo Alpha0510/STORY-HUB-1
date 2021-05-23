e53bcc60d2a76635131c406695ad70e725507adb
gaaurav <70964482+Alpha0510@users.noreply.github.com>
2021-05-23 18:31:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase'
+// require('@firebase/firestore')
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEAd6rUFJuv5tojEpXwcFMFFqN3-_JVfs",
+  authDomain: "story-hub-2b086.firebaseapp.com",
+  projectId: "story-hub-2b086",
+  storageBucket: "story-hub-2b086.appspot.com",
+  messagingSenderId: "310592940329",
+  appId: "1:310592940329:web:85d6a8dcf6d6976cd56f38"
+};
+  // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase.firestore();
